279221cdfaa3deef138ed8aa98ba0af1ffadc9ea
Md Imran <craftsbyimran@gmail.com>
2025-05-05 19:11:17 +0600

    jkh

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -35,6 +35,7 @@ const HeroSection = () => {
               </Button>
             </motion.div>
           </div>
+        
           {/* <div className="mt-8 flex items-center text-sm text-fintech-mediumGray">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 text-pink-500"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" /><polyline points="22 4 12 14.01 9 11.01" /></svg>
             <span>No Subscriptions Required.</span>
